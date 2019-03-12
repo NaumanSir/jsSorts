08adfaa13ff50538d21b07d63f6a2a852e3406a6
Nauman Husnani <naumanhusnani@gmail.com>
2019-03-11 20:28:32 -0500

    having trouble with bubble sort when I wasn't expecting to

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,8 +2,13 @@ var arr = []
 
 function bubbleSort {
     for(var i=0; i<arr.length; i++) {
-        for(var j=0; j<1; j--) {
-            if
+        for(var j=1; j<arr.length; j++) {
+            if(arr[j-1] > arr[j]) {
+                var temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
         }
     }
+    return arr;
 }
